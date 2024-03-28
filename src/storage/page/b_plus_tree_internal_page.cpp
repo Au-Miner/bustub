0ed212788e47123f735251d9632d70ede97a7d27
au_miner <358671982@qq.com>
2024-03-28 19:33:57 +0800

    1. project2 task1 over!

--- src/storage/page/b_plus_tree_internal_page.cpp
+++ src/storage/page/b_plus_tree_internal_page.cpp
@@ -24,33 +24,42 @@ namespace bustub {
  * Including set page type, set current size, set page id, set parent id and set
  * max page size
  */
-INDEX_TEMPLATE_ARGUMENTS
-void B_PLUS_TREE_INTERNAL_PAGE_TYPE::Init(page_id_t page_id, page_id_t parent_id, int max_size) {}
+    INDEX_TEMPLATE_ARGUMENTS
+    void B_PLUS_TREE_INTERNAL_PAGE_TYPE::Init(page_id_t page_id, page_id_t parent_id, int max_size) {
+        SetPageType(IndexPageType::INVALID_INDEX_PAGE);
+        SetSize(0);
+        SetPageId(page_id);
+        SetParentPageId(parent_id);
+        SetMaxSize(max_size);
+    }
 /*
  * Helper method to get/set the key associated with input "index"(a.k.a
  * array offset)
  */
-INDEX_TEMPLATE_ARGUMENTS
-auto B_PLUS_TREE_INTERNAL_PAGE_TYPE::KeyAt(int index) const -> KeyType {
-  // replace with your own code
-  KeyType key{};
-  return key;
-}
+    INDEX_TEMPLATE_ARGUMENTS
+    auto B_PLUS_TREE_INTERNAL_PAGE_TYPE::KeyAt(int index) const -> KeyType {
+        // replace with your own code
+        return array_[index].first;
+    }
 
-INDEX_TEMPLATE_ARGUMENTS
-void B_PLUS_TREE_INTERNAL_PAGE_TYPE::SetKeyAt(int index, const KeyType &key) {}
+    INDEX_TEMPLATE_ARGUMENTS
+    void B_PLUS_TREE_INTERNAL_PAGE_TYPE::SetKeyAt(int index, const KeyType &key) {
+        array_[index].first = key;
+    }
 
 /*
  * Helper method to get the value associated with input "index"(a.k.a array
  * offset)
  */
-INDEX_TEMPLATE_ARGUMENTS
-auto B_PLUS_TREE_INTERNAL_PAGE_TYPE::ValueAt(int index) const -> ValueType { return 0; }
+    INDEX_TEMPLATE_ARGUMENTS
+    auto B_PLUS_TREE_INTERNAL_PAGE_TYPE::ValueAt(int index) const -> ValueType {
+        return array_[index].second;
+    }
 
 // valuetype for internalNode should be page id_t
-template class BPlusTreeInternalPage<GenericKey<4>, page_id_t, GenericComparator<4>>;
-template class BPlusTreeInternalPage<GenericKey<8>, page_id_t, GenericComparator<8>>;
-template class BPlusTreeInternalPage<GenericKey<16>, page_id_t, GenericComparator<16>>;
-template class BPlusTreeInternalPage<GenericKey<32>, page_id_t, GenericComparator<32>>;
-template class BPlusTreeInternalPage<GenericKey<64>, page_id_t, GenericComparator<64>>;
+    template class BPlusTreeInternalPage<GenericKey<4>, page_id_t, GenericComparator<4>>;
+    template class BPlusTreeInternalPage<GenericKey<8>, page_id_t, GenericComparator<8>>;
+    template class BPlusTreeInternalPage<GenericKey<16>, page_id_t, GenericComparator<16>>;
+    template class BPlusTreeInternalPage<GenericKey<32>, page_id_t, GenericComparator<32>>;
+    template class BPlusTreeInternalPage<GenericKey<64>, page_id_t, GenericComparator<64>>;
 }  // namespace bustub
--- src/storage/page/b_plus_tree_leaf_page.cpp
+++ src/storage/page/b_plus_tree_leaf_page.cpp
@@ -26,32 +26,37 @@ namespace bustub {
  * Including set page type, set current size to zero, set page id/parent id, set
  * next page id and set max size
  */
-INDEX_TEMPLATE_ARGUMENTS
-void B_PLUS_TREE_LEAF_PAGE_TYPE::Init(page_id_t page_id, page_id_t parent_id, int max_size) {}
+    INDEX_TEMPLATE_ARGUMENTS
+    void B_PLUS_TREE_LEAF_PAGE_TYPE::Init(page_id_t page_id, page_id_t parent_id, int max_size) {
+        SetPageType(IndexPageType::LEAF_PAGE);
+        SetSize(0);
+        SetPageId(page_id);
+        SetParentPageId(parent_id);
+        SetMaxSize(max_size);
+    }
 
 /**
  * Helper methods to set/get next page id
  */
-INDEX_TEMPLATE_ARGUMENTS
-auto B_PLUS_TREE_LEAF_PAGE_TYPE::GetNextPageId() const -> page_id_t { return INVALID_PAGE_ID; }
+    INDEX_TEMPLATE_ARGUMENTS
+    auto B_PLUS_TREE_LEAF_PAGE_TYPE::GetNextPageId() const -> page_id_t { return next_page_id_; }
 
-INDEX_TEMPLATE_ARGUMENTS
-void B_PLUS_TREE_LEAF_PAGE_TYPE::SetNextPageId(page_id_t next_page_id) {}
+    INDEX_TEMPLATE_ARGUMENTS
+    void B_PLUS_TREE_LEAF_PAGE_TYPE::SetNextPageId(page_id_t next_page_id) { next_page_id_ = next_page_id; }
 
 /*
  * Helper method to find and return the key associated with input "index"(a.k.a
  * array offset)
  */
-INDEX_TEMPLATE_ARGUMENTS
-auto B_PLUS_TREE_LEAF_PAGE_TYPE::KeyAt(int index) const -> KeyType {
-  // replace with your own code
-  KeyType key{};
-  return key;
-}
+    INDEX_TEMPLATE_ARGUMENTS
+    auto B_PLUS_TREE_LEAF_PAGE_TYPE::KeyAt(int index) const -> KeyType {
+        // replace with your own code
+        return array_[index].first;
+    }
 
-template class BPlusTreeLeafPage<GenericKey<4>, RID, GenericComparator<4>>;
-template class BPlusTreeLeafPage<GenericKey<8>, RID, GenericComparator<8>>;
-template class BPlusTreeLeafPage<GenericKey<16>, RID, GenericComparator<16>>;
-template class BPlusTreeLeafPage<GenericKey<32>, RID, GenericComparator<32>>;
-template class BPlusTreeLeafPage<GenericKey<64>, RID, GenericComparator<64>>;
+    template class BPlusTreeLeafPage<GenericKey<4>, RID, GenericComparator<4>>;
+    template class BPlusTreeLeafPage<GenericKey<8>, RID, GenericComparator<8>>;
+    template class BPlusTreeLeafPage<GenericKey<16>, RID, GenericComparator<16>>;
+    template class BPlusTreeLeafPage<GenericKey<32>, RID, GenericComparator<32>>;
+    template class BPlusTreeLeafPage<GenericKey<64>, RID, GenericComparator<64>>;
 }  // namespace bustub
